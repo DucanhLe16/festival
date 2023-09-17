2e6397ecd0622f1d28d87930ecf4e9591a99d816
Le Duc Anh <127206760+DucanhLe16@users.noreply.github.com>
2023-09-17 17:37:20 +0700

    put some changes in java file

--- ehe.java
+++ ehe.java
@@ -1,1 +1,65 @@
-ạiddsd
+package org.apt.model;
+
+import org.bson.codecs.pojo.annotations.BsonProperty;
+import org.bson.types.ObjectId;
+
+public class Student {
+
+    private ObjectId id;
+    @BsonProperty("roll")
+    private String maSv;
+
+    @BsonProperty("full_name")
+    private String hoVaten;
+
+    @BsonProperty("class_name")
+    private String tenLop;
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getMaSv() {
+        return maSv;
+    }
+
+    public void setMaSv(String maSv) {
+        this.maSv = maSv;
+    }
+
+    public String getHoVaten() {
+        return hoVaten;
+    }
+
+    public void setHoVaten(String hoVaten) {
+        this.hoVaten = hoVaten;
+    }
+
+    public String getTenLop() {
+        return tenLop;
+    }
+
+    public void setTenLop(String tenLop) {
+        this.tenLop = tenLop;
+    }
+
+    public Student(String maSv, String hoVaten, String tenLop) {
+        this.maSv = maSv;
+        this.hoVaten = hoVaten;
+        this.tenLop = tenLop;
+    }
+
+    public Student() {
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "id=" + id + ", maSv=" + maSv + ", hoVaten=" + hoVaten + ", tenLop=" + tenLop + '}';
+    }
+
+    
+}
